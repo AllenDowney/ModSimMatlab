b1f7a593f44fdf1ca258d642fcc5db083ce46257
Allen Downey <downey@allendowney.com>
2014-12-01 13:42:50 -0500

    Improving code quality

--- code/Tutorial10/hopper.m
+++ code/Tutorial10/hopper.m
@@ -1,59 +1,73 @@
 
-function [t,Y]=hopper(m1,m2) % calculate the trajectory of the hopper for given masses of the post and the reaction mass
+function [t, M] = hopper(m1, m2) 
+% calculate the trajectory of the hopper for given masses of the 
+% post (m1) and the reaction mass (m2) in kg
+
 
 % Define constants for the hopper
-k=1000 % spring constant
-l=0 % rest length of spring
-R=1 % radius of disk (drives angle of spring)
+k = 500 % spring constant in N/m
+l = 0 % rest length of spring in m
+R = .1 % radius of disk in m (drives angle of spring)
 
 % Set the events to capture end of simulation condition
-options=odeset('Events',@events); %Stop when we hit the ground
+options = odeset('Events', @events); %Stop when we hit the ground
 
 % Set initial conditions
-X=[0;0;0;-1;0;0;0;0]; % [postx posty reactionmassx reactionmassy vpostx vposty etc]
+% [postx posty reactionmassx reactionmassy vpostx vposty etc]
+init = [0; 0; 0; -.1; 0; 0; 0; 0];
 
 % Calculate the trajectory, with a fixed timestep to facilitate animations.
-[t,Y]=ode45(@dHopperdt,[0:0.003:10],X,options);  
+[t, M] = ode45(@dHopperdt, [0:0.0003:2], init, options);  
 
 
-    function res=dHopperdt(t,X) % Function that passes back derivatives based on position and velocity
+    function res = dHopperdt(t, X) 
+    % Function that passes back derivatives based on position and velocity
 
-    g=10;
-    r1=X(1:2);
-    r2=X(3:4);
-    v1=X(5:6);
-    v2=X(7:8);
+    g = 10;   % acceleration of gravity in m/s^2
+    
+    % extract position of the two elements as 2-D vectors
+    r1 = X(1:2);
+    r2 = X(3:4);
+    
+    % extract velocity of the two elements as 2-D vectors
+    v1 = X(5:6);
+    v2 = X(7:8);
 
     % Deal with the rod normal force possibilities
-    if r1(2)<=0
-        ay1=max(-springforce(r1,r2)/m1 - g,0); % make sure that if post is on the ground, it does not move unless pulled up.
-    else
-        ay1=-springforce(r1,r2)/m1 - g;
+    % make sure that if post is on the ground, it does not move 
+    % unless pulled up.
+    force = springforce(r1,r2);
+    ay1 = -force / m1 - g;
+    if r1(2) <= 0 && ay1 < 0
+        ay1 = 0;
     end
 
     % Deal with the reaction mass    
-    ay2=springforce(r1,r2)/m2 - g; 
-    ax1=0;
-    ax2=0;
+    ay2 = force / m2 - g; 
+    ax1 = 0;
+    ax2 = 0;
 
     % Pass out the results
-    res=[v1;v2;ax1;ay1;ax2;ay2]; % note that v1 and v2 are each two elements.
+    % note that v1 and v2 are each two elements.
+    res = [v1; v2; ax1; ay1; ax2; ay2];
 
 
-        function res=springforce(r1,r2)
+        function res = springforce(r1, r2)
             % simple function to calculate the force in the y direction due
             % to the angled springs
         
-            y1=r1(2);
-            y2=r2(2);
-            res=k*(sqrt(R^2+(y1-y2)^2)-l)*(y1-y2)/sqrt(R^2+(y1-y2)^2);
+            y1 = r1(2);
+            y2 = r2(2);
+            L = sqrt(R^2 + (y1-y2)^2);
+            res = k* (L-l) * (y1-y2) / L;
         end
-
-
     end
-    function [value, isterminal,direction] = events(t,X)
-            value=X(2)+0.1; % Check if we hit the ground (actually a little below it to avoid errors associated with the if statement)
-            isterminal=1;
-            direction=-1;
+
+    function [value, isterminal, direction] = events(t,X)
+        % Check if we hit the ground (actually a little below it to avoid
+        % floating point issues)
+        value = X(2) + 0.01; 
+        isterminal = 1;
+        direction = -1;
     end
 end
--- code/Tutorial10/hopperanimation.m
+++ code/Tutorial10/hopperanimation.m
@@ -5,19 +5,32 @@
 % It calls hopper.m, which takes the two masses as input, and returns the
 % trajectory information.
 close all
-m1=1;
-m2=1;
+m1 = .01;    % mass in kg
+m2 = .1;    % mass in kg
 figure;
-[t,Y]=hopper(m1,m2);
-y=Y(1,:)
-h1=rectangle('Position',[y(1),y(2),0.1,1],'FaceColor',[0 0 1]) % create rectangles
-h2=rectangle('Position',[y(3)-0.5,y(4)+1,1,0.1],'FaceColor',[1 0 0])
-axis([-8 8 0 16])
+
+[t, M] = hopper(m1, m2);
+r = M(1, :);
+
+% create rectangles
+narrow = 0.01;
+wide = .1;
+
+shiftx = -wide/2;     % shiftx offsets for the rectangle representing the reaction mass;
+shifty = wide;        % shifty accounts for the fact that reaction mass position is zero at the initial 
+                      % height of the hopper (i.e., negative when loaded)
+
+h1 = rectangle('Position', [r(1), r(2), narrow, wide], ...
+                'FaceColor', 'blue');
+h2 = rectangle('Position', [r(3)+shiftx, r(4)+shifty, wide, narrow], ...
+                'FaceColor', 'red');
+axis([-1 1 0 2])
 
 for i=1:length(t);
-    
-    set(h1,'Position',[Y(i,1)-0.05,Y(i,2),0.1,1]); % change position of rectangles
-    set(h2,'Position',[Y(i,3)-0.5,Y(i,4)+1,1,0.1]);
+    % change position of rectangles
+    r = M(i, :);
+    set(h1,'Position', [r(1), r(2), narrow, wide]);
+    set(h2,'Position', [r(3)+shiftx, r(4)+shifty, wide, narrow]);
     drawnow; % wait a little while so that the animation is visible
 end
 
